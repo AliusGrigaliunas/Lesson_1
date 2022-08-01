a650abd3929aa02fd403d051afa3b243a3b8d1c6
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-08-01 16:47:52 +0300

    feat(63):added public/js/main.js file, add main.ts and index.html

---
+++ Lesson_63/public/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+const calc = (a, b) => a * b;
+let a = 5;
+let b = 6;
+console.log({
+    'Daugyba 5*6:': calc(a, b),
+    'Daugybe 9*9': calc(9, 9),
+    'Wow': calc(6, 6)
+});
+//# sourceMappingURL=main.js.map
---
+++ Lesson_63/src/main.ts
@@ -0,0 +1,12 @@
+
+const calc = (a:number,b:number):number => a*b;
+let a: number = 5;
+let b: number = 6;
+
+console.log({
+    'Daugyba 5*6:':calc(a,b),
+    'Daugybe 9*9':calc(9,9),
+    'Wow':calc(6,6)
+});
+
+
